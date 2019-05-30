88be6e63f99d2ef9ba31f56d517759c7adb951bb
frankkarro <frank.karro@khk.ee>
2019-05-30 13:11:12 +0300

    Update yl2.sh

--- praks2/yl2.sh
+++ praks2/yl2.sh
@@ -3,8 +3,8 @@ Loo skript mis võtab sisendina kasutajanimed eraldi failist ning selle alusel l
 Skript:
 #!/bin/bash
 #skript lisamaks kasutajaid failist
-for a in $(cat /home/frank/skriptid/praks3/newusers.lst)
-do
-    echo "$a added to the system"
-    sudo useradd -m -s /bin/bash $a
+for a in $(cat /home/frank/skriptid/praks3/newusers.lst) #annan ette faili mida töödelda
+do 
+    sudo useradd -m -s /bin/bash $a  #lisan kasutaja koos skeletiga
+    echo "$a added to the system" #annan teate töö lõpetamisest
 done
