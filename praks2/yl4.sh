f4d01ed097c9aa5f1c1848aa712f1efcd5c6c35c
frankkarro <frank.karro@khk.ee>
2019-05-30 13:26:25 +0300

    Update yl4.sh

--- praks2/yl4.sh
+++ praks2/yl4.sh
@@ -1,1 +1,7 @@
-Ei ole jõudnud teha. Teised jätsid 5nda tegemata, ma jätan siis neljanda kui tohib. 
+#!/bin/bash
+echo -n "Sisesta kasutaja nimede faili nimi:" 
+read $kasutaja
+echo -n "Sisesta paroolidega faili nimi:"
+read $parool
+useradd -m $kasutaja
+echo $kasutaja:$parool | chpasswd
