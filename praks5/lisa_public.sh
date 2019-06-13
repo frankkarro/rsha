eb29ad6f079a30325c1771995fc6104d750bf5f1
frankkarro <frank.karro@khk.ee>
2019-06-14 01:06:10 +0300

    Create lisa_public.sh

---
+++ praks5/lisa_public.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#kui pole kasutajanime skripti järel siis annab infot kuidas skripti jooksutada
+if [ $# -ne 1 ]; then
+        echo "Kasutusjuhend: $0 kasutajanimi"
+else    #kui kasutajat pole eelnevalt süsteemis olemas siis loob uue kasutaja (kasutajanimi:qwerty) paroolida
+        #teeb kasutajale skeletoni ning määrab public kausta õigused
+        kasutajanimi=$1
+        useradd $kasutajanimi -d /home/$kasutajanimi/ -s /bin/bash -p qwerty
+        mkdir -p /home/$kasutajanimi/public && chown -R $kasutajanimi:$kasutajanimi /home/$kasutajanimi/public && chmod 751 /home/$
+
+        if [ $? -eq 0 ]; then   #vigadeta töö korral väljastab:
+                echo "Kasutaja $kasutajanimi  kodukaustas loodud kaust public"
+                cat /etc/passwd | grep $kasutajanimi #kontrollib kasutaja olemasolu passwd failis
+        else
+                echo "probleem kasutaja $kasutajanimi lisamisega" #veateade
+                echo "probleemi kood on $?" #veateate tunnus
+        fi
+fi
